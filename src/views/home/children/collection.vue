299f9f7c253e76e011e862dab9db7cc1a40ad857
zhang <15918862238@163.com>
2018-04-23 10:46:08 +0800

    初始化

---
+++ src/views/home/children/collection.vue
@@ -0,0 +1,40 @@
+<!--我的收藏-->
+<template>
+  <div class="collection">
+    <v-head title_head="我的收藏" goBack="true" bgColor="#f4f4f4"></v-head>
+    <div class="info_container">
+      <img src="../../../assets/nothing.png">
+      <span class="text">没有收藏的你一定是个假吃货</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .collection {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: #fff;
+    .info_container {
+      text-align: center;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      img {
+        width: 3rem;
+        height: 3rem;
+      }
+      .text {
+        display: block;
+        font-size: 0.35rem;
+      }
+    }
+  }
+</style>
